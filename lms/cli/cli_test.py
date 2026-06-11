b8da7987a762867e31202a671c7f1d8b2a56fa2e
Eriq Augustine <eriq.augustine@gmail.com>
2026-06-11 13:01:41 -0500

    Moved config CLI tests to the same dir. [skip ci]

--- lms/cli/cli_test.py
+++ lms/cli/cli_test.py
@@ -11,5 +11,11 @@ DATA_DIR: str = os.path.join(BASE_TESTDATA_DIR, "data")
 class CLITest(edq.testing.unittest.BaseTest):
     """ Test CLI invocations (without a backing server). """
 
+    @classmethod
+    def get_test_basename(cls, path: str) -> str:
+        """ Get the test's name based off of its filename and location. """
+
+        return edq.testing.cli.compute_ancestor_basename(path, TEST_CASES_DIR)
+
 # Populate CLITest with all the test methods.
 edq.testing.cli.discover_test_cases(CLITest, TEST_CASES_DIR, DATA_DIR)
